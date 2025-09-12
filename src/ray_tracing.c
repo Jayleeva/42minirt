a1b5bad2ebdad0d938cd439b6a49b7716687db66
Cynthia Glardon <cyglardo@c3r3s2.42lausanne.ch>
2025-09-12 13:58:59 +0200

    image

--- src/ray_tracing.c
+++ src/ray_tracing.c
@@ -12,13 +12,15 @@ void	cast_ray(t_data *data, int i, int x, int y)
 
 int ray_tracing(t_data *data)
 {
-	int	x;
-	int	y;
-	int	i;
+	int		x;
+	int		y;
+	int		i;
+	void	*img_ptr;
 
 	data->canvas = malloc((W_HEIGHT * W_WIDTH + 1) * sizeof(t_pixel));
 	if (!data->canvas)
 		return (0);
+	mlx_new_image(data->mlx_ptr, W_WIDTH, W_HEIGHT);
 	i = 0;
 	x = 0;
 	while (x < W_WIDTH)
@@ -29,9 +31,8 @@ int ray_tracing(t_data *data)
 			cast_ray(data, i, x, y);
 			//compute_intersections(data, x, y);
 			//set_color(data, x, y);
-			printf("pixel color = %X\n", data->canvas[i].color);
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, data->canvas[i].color);
-			printf("hello segfault\n");
+			//mlx_get_data_addr(void *img_ptr, int *bits_per_pixel, int *size_line, int *endian);
+			//mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, data->canvas[i].color);
 			i ++;
 			y ++;
 		}
@@ -39,5 +40,6 @@ int ray_tracing(t_data *data)
 	}
 	//au lieu de mlx_pixel_put(), changer le canevas en image xpm puis utiliser mlx_xmp_file_to_img() puis load_img() puis put_image_to_window()?
 	//utiliser format ppm P6? comment faire le lien avec la minilibx?
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img_ptr, 0, 0);
 	return (1);
 }
