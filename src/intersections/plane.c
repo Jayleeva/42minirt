4d27e77e236865843486bde2bd5eb38875d60f63
Cynthia Glardon <cy.glardon@gmail.com>
2025-09-19 15:49:27 +0200

    started plane

--- src/intersections/plane.c
+++ src/intersections/plane.c
@@ -2,6 +2,12 @@
 
 int	hit_plane(const t_ray *r, const t_pl *pl, float tmin, float tmax, t_hit *out)
 {
+    t_vector	oc;
+    float   t;
+
+    oc = v_from_points(pl->coord, r->o); // calcule distance du point d'origine au sommet c du triangle (car 3 dim) = hypothemuse.
+
+    out->t = t;
     out->kind = PLANE;
     return (1);
 }
