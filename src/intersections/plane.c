7d124c0e699a2904d03fc0c07d0ff650cf50e412
Cynthia Glardon <cyglardo@c1r14s7.42lausanne.ch>
2025-09-30 13:19:27 +0200

    tentative...oskour....

--- src/intersections/plane.c
+++ src/intersections/plane.c
@@ -2,12 +2,34 @@
 
 int	hit_plane(const t_ray *r, const t_pl *pl, float tmin, float tmax, t_hit *out)
 {
-    t_vector	oc;
-    float   t;
+    t_ray       neg_o;
+    t_vector	neg_oc;
+    float       t;
 
-    oc = v_from_points(pl->coord, r->o); // calcule distance du point d'origine au sommet c du triangle (car 3 dim) = hypothemuse.
+    // tout mettre en negatif pour la suite
+    neg_o.o.x = r->o.x * -1;
+    neg_o.o.y = r->o.y * -1;
+    neg_o.o.z = r->o.z * -1;
+    neg_oc = v_from_points(pl->coord, neg_o.o);
+
+    if (v_dot(r->d, V) == 0)
+        return (0); // impossible de diviser par 0, donc on s'arrete la
+    //t = dot product de -X (- Origine - Centre) et V divise par dot product de Direction et V
+    t = v_dot(neg_oc, V) / v_dot(r->d, V); // si different de 0, on calcule le t.
+
+    // verifier si t est dans les bornes, s'il n'y est pas, ne sera pas affiche
+    if (t < tmin || t > tmax)
+        return (0); // n'est pas dans les bornes
+
+    // if dot product de (P-C, ou (O + t*D) - C) et V == 0, intersection
+    if (v_dot((r->o + t * r->d) - C, V) != 0)
+        return (0); // pas d'intersection
 
     out->t = t;
+    out->p = p_add_v(r->o, v_scale(r->d, t)); // point d’impact : origine + t*direction
+	out->n = v_norm(v_from_points(pl->coord, out->p)); // normale = (P - centre)/|...|
+	if (v_dot(out->n, r->d) > 0.0f) // si normale inversee, reinverser
+		out->n = v_scale(out->n, -1.0f);
     out->kind = PLANE;
     return (1);
 }
