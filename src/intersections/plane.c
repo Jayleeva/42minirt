b6695c708314fd24bfd0f6ee2b5c3e2bfd04f28a
Cynthia Glardon <cyglardo@c1r13s7.42lausanne.ch>
2025-11-25 14:13:29 +0100

    reworked plane, still weird

--- src/intersections/plane.c
+++ src/intersections/plane.c
@@ -2,43 +2,67 @@
 
 int	hit_plane(const t_ray *r, const t_pl *pl, float tmin, float tmax, t_hit *out)
 {
-	t_ray		neg;
-	t_vector	neg_oc;
-	float		t;
-	t_vector	tmp;
+	float	t;
 
-	// mettre coord de Origine en negatif pour la suite
-	neg.o.x = r->o.x * -1;
-	neg.o.y = r->o.y * -1;
-	neg.o.z = r->o.z * -1;
-	neg_oc = v_from_points(pl->coord, neg.o);
+	if (r->d.o_y < EPS)
+		return (0); // pas d'intersection car paralleles ou coplanaire
 
-	if (v_dot(r->d, pl->ornt) == 0)
-		return (0); // impossible de diviser par 0, donc on s'arrete la
+	t = (r->o.y * -1) / r->d.o_y;
 
-	//t = dot product de -X (- Origine - Centre) et V divise par dot product de Direction et V
-	t = v_dot(neg_oc, pl->ornt) / v_dot(r->d, pl->ornt); // si different de 0, on calcule le t.
+	/*Scenario: A ray intersecting a plane from above
+	Given p ← plane()
+	And r ← ray(point(0, 1, 0), vector(0, -1, 0))
+	When xs ← local_intersect(p, r)
+	Then xs.count = 1
+	And xs[0].t = 1
+	And xs[0].object = p
+	Scenario: A ray intersecting a plane from below
+	Given p ← plane()
+	And r ← ray(point(0, -1, 0), vector(0, 1, 0))
+	When xs ← local_intersect(p, r)
+	Then xs.count = 1
+	And xs[0].t = 1
+	And xs[0].object = p*/
 
-	// verifier si t est dans les bornes, s'il n'y est pas, ne sera pas affiche
-	/*if (t < tmin || t > tmax)
-		return (0); // n'est pas dans les bornes*/
 	(void)tmin;
 	(void)tmax;
-
-	// precalculer un tmp pour le v_dot
-	tmp.o_x = (r->o.x + r->d.o_x * t) - pl->coord.x;
-	tmp.o_y = (r->o.y + r->d.o_y * t) - pl->coord.y;
-	tmp.o_z = (r->o.z + r->d.o_z * t) - pl->coord.z;
-
-	// if dot product de (P-C, ou (O + t*D) - C) et V == 0, intersection
-	if (v_dot(tmp, pl->ornt) != 0)
-		return (0); // pas d'intersection
-
+	/*if (t < tmin || t > tmax)
+		return (0);*/ // n'est pas dans les bornes (besoin?)
+		
 	out->t = t;
-	out->p = p_add_v(r->o, v_scale(r->d, t)); // point d’impact : origine + t*direction
-	out->n = v_norm(v_from_points(pl->coord, out->p)); // normale = (P - centre)/|...|
+	out->p = p_add_v(r->o, v_scale(r->d, t));
+	out->n = v_norm(v_from_points(pl->coord, out->p)); // manque prise en compte de l'orientation?
 	if (v_dot(out->n, r->d) > 0.0f) // si normale inversee, reinverser
 		out->n = v_scale(out->n, -1.0f);
 	out->kind = PLANE;
 	return (1);
 }
+
+/*The logic to intersect a ray with a plane is the only other bit that needs
+implementing, and it has four cases to consider:
+1. The ray is parallel to the plane, and will thus never intersect it.
+2. The ray is coplanar with the plane, which is to say that the ray’s origin is
+on the plane, and the ray’s direction is parallel to the plane. You’re viewing
+the plane edge-on. In this case, every point on the ray intersects the plane,
+resulting in an infinite number of intersections. That’s unwieldy! But
+since a plane is infinitely thin, it’s invisible when viewed like this, so we’ll
+assume the ray misses in this case.
+3. The ray origin is above the plane.
+4. The ray origin is below the plane.
+
+
+implement the following formula for computing the intersection of a ray with a plane.
+Note that this formula only works if the plane is as described above—in xz, with the normal pointing in the positive y direction.
+
+t = −origin y
+____________
+direction y
+
+The variable origin is the ray’s origin, and direction is the ray’s direction vector.
+The following pseudocode shows how the complete local_intersect() function might look.
+
+function local_intersect(ray, plane)
+	if abs(ray.direction.y) < EPSILON
+		return () # empty set -- no intersections
+	t ← -ray.origin.y / ray.direction.y
+	return ( intersection(t, plane) )*/
