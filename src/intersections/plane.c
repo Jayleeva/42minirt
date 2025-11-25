78566a3541d714a37b79ed97c04a2d63a79f8c9c
Cynthia Glardon <cyglardo@c1r13s7.42lausanne.ch>
2025-11-25 14:48:21 +0100

    planes a bit less weird but still weird

--- src/intersections/plane.c
+++ src/intersections/plane.c
@@ -3,35 +3,32 @@
 int	hit_plane(const t_ray *r, const t_pl *pl, float tmin, float tmax, t_hit *out)
 {
 	float	t;
+	t_vector X;
+	float    dv, xv, m;
 
 	if (r->d.o_y < EPS)
 		return (0); // pas d'intersection car paralleles ou coplanaire
 
 	t = (r->o.y * -1) / r->d.o_y;
 
-	/*Scenario: A ray intersecting a plane from above
-	Given p ← plane()
-	And r ← ray(point(0, 1, 0), vector(0, -1, 0))
-	When xs ← local_intersect(p, r)
-	Then xs.count = 1
-	And xs[0].t = 1
-	And xs[0].object = p
-	Scenario: A ray intersecting a plane from below
-	Given p ← plane()
-	And r ← ray(point(0, -1, 0), vector(0, 1, 0))
-	When xs ← local_intersect(p, r)
-	Then xs.count = 1
-	And xs[0].t = 1
-	And xs[0].object = p*/
+	//(void)tmin;
+	//(void)tmax;
+	if (t < tmin || t > tmax)
+		return (0); // n'est pas dans les bornes (besoin?)
 
-	(void)tmin;
-	(void)tmax;
-	/*if (t < tmin || t > tmax)
-		return (0);*/ // n'est pas dans les bornes (besoin?)
-		
+	X = v_from_points(pl->coord, r->o);
+	dv = v_dot(r->d, pl->ornt);
+	xv = v_dot(X, pl->ornt);
+	m = dv * t + xv;
+
 	out->t = t;
 	out->p = p_add_v(r->o, v_scale(r->d, t));
-	out->n = v_norm(v_from_points(pl->coord, out->p)); // manque prise en compte de l'orientation?
+
+	out->n = v_from_points(pl->coord, out->p);
+	out->n = v_sub(out->n, v_scale(pl->ornt, m));
+	out->n = v_norm(out->n);
+
+	//out->n = v_norm(v_from_points(pl->coord, out->p)); // manque prise en compte de l'orientation?
 	if (v_dot(out->n, r->d) > 0.0f) // si normale inversee, reinverser
 		out->n = v_scale(out->n, -1.0f);
 	out->kind = PLANE;
