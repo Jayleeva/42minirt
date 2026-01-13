23626d7ae979b56875599aa7e2c41ec432919975
Cynthia Glardon <cyglardo@c1r16s7.42lausanne.ch>
2026-01-13 11:18:22 +0100

    no more leaks

--- src/parsing/config_lower.c
+++ src/parsing/config_lower.c
@@ -4,21 +4,23 @@
 int	check_sp(t_data *data, char *sp)
 {
 	char	**tab;
+	char	*trim;
 
 	tab = ft_split(sp, ' ');
 	if (!tab)
 		return (0);
 	if (!tab[3] || tab[4])
 		return (free_tab(tab), 0);
-	tab[3] = ft_strtrim(tab[3], "\n");
 	if (!check_coord(&(data->sp[data->i_sp].coord), tab[1]))
 		return (free_tab(tab), 0);
 	if (!check_diameter_or_height(&(data->sp[data->i_sp].diameter), tab[2]))
 		return (free_tab(tab), 0);
-	if (!check_colors(&(data->sp[data->i_sp].colors), tab[3]))
-		return (free_tab(tab), 0);
+	trim = ft_strtrim(tab[3], "\n");
+	if (!check_colors(&(data->sp[data->i_sp].colors), trim))
+		return (free_tab(tab), free(trim), 0);
 	data->i_sp ++;
 	free_tab(tab);
+	free(trim);
 	printf("check sp : OK\n");
 	return (1);
 }
@@ -27,21 +29,23 @@ int	check_sp(t_data *data, char *sp)
 int	check_pl(t_data *data, char *pl)
 {
 	char	**tab;
+	char	*trim;
 
 	tab = ft_split(pl, ' ');
 	if (!tab)
 		return (0);
 	if (!tab[3] || tab[4])
 		return (free_tab(tab), 0);
-	tab[3] = ft_strtrim(tab[3], "\n");
 	if (!check_coord(&(data->pl[data->i_pl].coord), tab[1]))
 		return (free_tab(tab), 0);
 	if (!check_ornt(&(data->pl[data->i_pl].ornt), tab[2]))
 		return (free_tab(tab), 0);
-	if (!check_colors(&(data->pl[data->i_pl].colors), tab[3]))
-		return (free_tab(tab), 0);
+	trim = ft_strtrim(tab[3], "\n");
+	if (!check_colors(&(data->pl[data->i_pl].colors), trim))
+		return (free_tab(tab), free(trim), 0);
 	data->i_pl ++;
 	free_tab(tab);
+	free(trim);
 	printf("check pl : OK\n");
 	return (1);
 }
@@ -50,13 +54,13 @@ int	check_pl(t_data *data, char *pl)
 int	check_cy(t_data *data, char *cy)
 {
 	char	**tab;
+	char	*trim;
 
 	tab = ft_split(cy, ' ');
 	if (!tab)
 		return (0);
 	if (!tab[5] || tab[6])
 		return (free_tab(tab), 0);
-	tab[5] = ft_strtrim(tab[5], "\n");
 	if (!check_coord(&(data->cy[data->i_cy].coord), tab[1]))
 		return (free_tab(tab), 0);
 	if (!check_ornt(&(data->cy[data->i_cy].ornt), tab[2]))
@@ -65,10 +69,12 @@ int	check_cy(t_data *data, char *cy)
 		return (free_tab(tab), 0);
 	if (!check_diameter_or_height(&(data->cy[data->i_cy].height), tab[4]))
 		return (free_tab(tab), 0);
-	if (!check_colors(&(data->cy[data->i_cy].colors), tab[5]))
-		return (free_tab(tab), 0);
+	trim = ft_strtrim(tab[5], "\n");
+	if (!check_colors(&(data->cy[data->i_cy].colors), trim))
+		return (free_tab(tab), free(trim), 0);
 	data->i_cy ++;
 	free_tab(tab);
+	free(trim);
 	printf("check cy : OK\n");
 	return (1);
 }
